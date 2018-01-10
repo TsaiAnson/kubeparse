4d603f3fd8832c37129a4950b8fc52536588c158
Yuan-Cheng Tsai <ansonytsai@gmail.com>
2018-01-09 21:55:54 -0800

    Created a second version of replicaUpdate UNTESTED

--- update.go
+++ update.go
@@ -9,6 +9,9 @@ import (
     "k8s.io/client-go/kubernetes"
     "k8s.io/client-go/tools/clientcmd"
     "k8s.io/client-go/util/retry"
+
+    //"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 func replicaUpdate(abspath string, metaname string, magnitude string) {
@@ -43,7 +46,56 @@ func replicaUpdate(abspath string, metaname string, magnitude string) {
 
         // Modify replica count
         oldRep := result.Spec.Replicas
-        result.Spec.Replicas = result.Spec.Replicas + int32(i)
+        result.Spec.Replicas = oldRep + int32(i)
+        if result.Spec.Replicas < 1 {
+            result.Spec.Replicas = 1
+        }
+        _, updateErr := deploymentsClient.Update(result)
+        return updateErr
+    })
+    if retryErr != nil {
+        panic(fmt.Errorf("Update failed: %v", retryErr))
+    }
+    fmt.Println("Updated replica count of Deployment %v from %v to %v", metaname, oldRep, result.Spec.Replicas)
+}
+
+func replicaUpdatev1(metaname string, magnitude string) {
+    config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	namespace := "default"
+	deploymentsClient := clientset.Extensions().Deployments(namespace)
+
+    // Updating deployment
+    retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+        // Retrieve the latest version of Deployment before attempting update
+        // RetryOnConflict uses exponential backoff to avoid exhausting the apiserver
+        result, getErr := deploymentsClient.Get(metaname, metav1.GetOptions{})
+        if getErr != nil {
+            panic(fmt.Errorf("Failed to get latest version of Deployment: %v", getErr))
+        }
+
+        fmt.Println("Updating replica count of %v by %v", metaname, magnitude)
+
+        // Parsing magnitude to int32
+        i, err := strconv.ParseInt(magnitude, 10, 32)
+        if err != nil {
+            panic(err)
+        }
+
+        // Modify replica count
+        oldRep := result.Spec.Replicas
+        result.Spec.Replicas = oldRep + int32(i)
+        if result.Spec.Replicas < 1 {
+            result.Spec.Replicas = 1
+        }
         _, updateErr := deploymentsClient.Update(result)
         return updateErr
     })
